65c525f49d0652d2119edd6447cc5d884e0db897
ncutixavier <ncuti60@gmail.com>
2022-03-04 00:45:46 +0200

    add layout

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchBar}>
+          <Text>Test App</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>Test App</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  searchBar: {
+    backgroundColor: "green",
+    padding: 10,
+  },
+  listContainer: {
+    backgroundColor: "blue",
+    flex: 1,
+    padding: 10,
+  }
 });
